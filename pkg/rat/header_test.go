eac2787a63ce68112512417a6dfc31f1a1318a4e
Tomas Sarquis <tsarquis88@gmail.com>
2024-10-11 13:01:21 +0200

    Add missing test

--- pkg/rat/header_test.go
+++ pkg/rat/header_test.go
@@ -48,6 +48,27 @@ func (suite *HeaderTestSuite) TestNewHeaderFromFile() {
 	assert.Equal(suite.T(), expectedVersion, header.version)
 }
 
+// NewHeaderFromDump()
+
+func (suite *HeaderTestSuite) TestNewHeaderFromDump() {
+	dumpData := []byte{102, 105, 108, 101, 46, 116, 101, 115, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 48, 48, 48, 48, 55, 53, 53, 0, 48, 48, 48, 49, 55, 53, 48, 0, 48, 48, 48, 49, 55, 53, 48, 0, 48, 48, 48, 48, 48, 48, 48, 49, 55, 53, 48, 0, 49, 52, 55, 48, 50, 50, 48, 50, 48, 50, 55, 0, 0, 0, 0, 0, 0, 0, 0, 0, 48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 117, 115, 116, 97, 114, 32, 32, 0, 116, 111, 109, 115, 97, 114, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 116, 111, 109, 115, 97, 114, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	header := NewHeaderFromDump(dumpData)
+
+	expectedName := FillWith([]byte(suite.fileName), 0, NameLen)
+	expectedMode := []byte{'0', '0', '0', '0', '7', '5', '5', 0}
+	expectedSize := []byte{'0', '0', '0', '0', '0', '0', '0', '1', '7', '5', '0', 0}
+	expectedTypeflag := uint8(RegulatFileType)
+	expectedMagic := []byte{'u', 's', 't', 'a', 'r', 32}
+	expectedVersion := []byte{32, 0}
+
+	assert.Equal(suite.T(), expectedName, header.name)
+	assert.Equal(suite.T(), expectedMode, header.mode)
+	assert.Equal(suite.T(), expectedSize, header.size)
+	assert.Equal(suite.T(), expectedTypeflag, header.typeflag)
+	assert.Equal(suite.T(), expectedMagic, header.magic)
+	assert.Equal(suite.T(), expectedVersion, header.version)
+}
+
 // TestHeaderTestSuite
 
 func TestHeaderTestSuite(t *testing.T) {
